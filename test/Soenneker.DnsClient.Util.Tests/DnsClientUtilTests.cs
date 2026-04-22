e089af68fe52128ec3f677e7fe15bfbccfa1be8a
Jake Soenneker <4441470+soenneker@users.noreply.github.com>
2026-04-21 19:01:36 -0500

    TUnit conversion

--- test/Soenneker.DnsClient.Util.Tests/DnsClientUtilTests.cs
+++ test/Soenneker.DnsClient.Util.Tests/DnsClientUtilTests.cs
@@ -2,23 +2,21 @@ using System.Threading.Tasks;
 using DnsClient;
 using AwesomeAssertions;
 using Soenneker.DnsClient.Util.Abstract;
-using Soenneker.Tests.FixturedUnit;
-using Xunit;
-
+using Soenneker.Tests.HostedUnit;
 
 namespace Soenneker.DnsClient.Util.Tests;
 
-[Collection("Collection")]
-public class DnsClientUtilTests : FixturedUnitTest
+[ClassDataSource<Host>(Shared = SharedType.PerTestSession)]
+public class DnsClientUtilTests : HostedUnitTest
 {
     private readonly IDnsClientUtil _util;
 
-    public DnsClientUtilTests(Fixture fixture, ITestOutputHelper output) : base(fixture, output)
+    public DnsClientUtilTests(Host host) : base(host)
     {
         _util = Resolve<IDnsClientUtil>(true);
     }
 
-    [Fact]
+    [Test]
     public async Task GetAddress_should_get_address()
     {
         LookupClient client = await _util.Get();
--- test/Soenneker.DnsClient.Util.Tests/Host.cs
+++ test/Soenneker.DnsClient.Util.Tests/Host.cs
@@ -1,15 +1,16 @@
+using System.Threading.Tasks;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Serilog;
 using Soenneker.DnsClient.Util.Registrars;
-using Soenneker.Fixtures.Unit;
+using Soenneker.TestHosts.Unit;
 using Soenneker.Utils.Test;
 
 namespace Soenneker.DnsClient.Util.Tests;
 
-public class Fixture : UnitFixture
+public class Host : UnitTestHost
 {
-    public override System.Threading.Tasks.ValueTask InitializeAsync()
+    public override Task InitializeAsync()
     {
         SetupIoC(Services);
 
